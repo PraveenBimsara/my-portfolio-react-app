a79b47e80db329a403728a9e47dfc5a4777ea19c
PraveenBimsara <praveenbimsara99@gmail.com>
2024-06-18 23:59:32 +0530

    Updated my projects

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -3,7 +3,7 @@ import project2_img from '../assets/project 2.jpg'
 import project3_img from '../assets/project 3.jpg'
 import project4_img from '../assets/project 4.png'
 import project5_img from '../assets/project 5.png'
-
+import project6_img from '../assets/project 6.png'
 
 
 const mywork_data = [
@@ -24,13 +24,18 @@ const mywork_data = [
     },
     {
         w_no:4,
-        w_name:"Shopping Mobile App - This is a E commerce app for sale shoes items.",
+        w_name:"Minimal Chat App - This app provides a simple, user-friendly interface for real-time messaging, built with Flutter for a smooth cross-platform experience and powered by Firebase for reliable backend support",
         w_img:project4_img
     },
     {
         w_no:5,
         w_name:"Food Delivering Web App - Through this project, I built a robust application that offers a smooth and engaging user experience, leveraging the power of React JS to create a responsive and interactive platform.",
         w_img:project5_img
+    },
+    {
+        w_no:6,
+        w_name:"Spotify Clone App - This app is a Spotify clone built with React, offering a sleek interface for music streaming and playlist management.",
+        w_img:project6_img
     },
     
 ]
